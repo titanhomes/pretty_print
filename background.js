60a4a0a29d78d2d8f1613554525b65cb4d618328
Kevin Korte <kevinkorte15@gmail.com>
2017-06-01 08:38:31 -0700

    cleaned up background.js

--- background.js
+++ background.js
@@ -1,9 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Turning '+tab.url+' red!');
-  // chrome.tabs.executeScript({
-  //   code: 'document.body.style.backgroundColor="red"'
-  // });
-  console.log(tab.id);
   chrome.tabs.insertCSS(tab.id, {
     file: 'pretty_print.css'
   });

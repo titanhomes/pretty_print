7e7aa829ef7d95b267510c05676845046915ae10
Kevin Korte <kevinkorte15@gmail.com>
2017-06-02 09:20:26 -0700

    fixed on builder side where javascript styles were remove selections

--- pretty_print.js
+++ pretty_print.js
@@ -1,4 +1,6 @@
 let element = document.getElementsByClassName('notChosen');
 Array.prototype.forEach.call(element, function(el, index, array) {
-  el.parentNode.style.display = "none"
+  if (el.parentNode.nodeName != "TBODY") {
+    el.parentNode.style.display = "none";
+  }
 })
